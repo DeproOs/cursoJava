b9b29bcf83a3f34efa5de66f60d139ca240ecbde
DeproOs <klaudio_4ever@hotmail.com>
2023-04-27 12:49:40 -0400

    clase 10 terminada

--- HolaMundo/src/HolaMundo.java
+++ HolaMundo/src/HolaMundo.java
@@ -1,5 +1,38 @@
+import java.util.Locale;
+
 public class HolaMundo {
     public static void main(String[] args){
-        System.out.println("Hola Mundo desde java");
+        String saludar = "Hola Mundo desde Java";
+        System.out.println(saludar);
+
+        System.out.println("saludar.toUpperCase() = " + saludar.toUpperCase());
+
+        int numero = 10;
+        System.out.println("numero = " + numero);
+
+        boolean valor = true;
+
+        int numero2 = 5;
+        if(valor){
+            System.out.println("numero = " + numero);
+            numero2 = 10;
+        }
+
+        System.out.println("numero2 = " + numero2);
+
+        var numero3 = "15";
+
+        System.out.println("numero3 = " + numero3);
+
+        String nombre;
+        nombre = "andres";
+        if(numero > 10){
+            nombre="juan";
+        }
+
+        System.out.println("nombre = " + nombre);
+
+        // int edadPersona = 5;
     }
+
 }
