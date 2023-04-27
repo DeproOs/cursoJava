965f972a2703af74dbb248748733e45404eae916
DeproOs <klaudio_4ever@hotmail.com>
2023-04-27 14:34:33 -0400

    clase 13 terminada

---
+++ HolaMundo/src/PrimitivosFloat.java
@@ -0,0 +1,9 @@
+public class PrimitivosFloat {
+    public static void main(String[] args) {
+
+
+        float realFloat = 1.5e-10F;//1.5e4F;//2.12e3F;//2120F;
+        System.out.println("realFloat = " + realFloat);
+
+    }
+}
