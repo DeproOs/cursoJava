d1074f8355258bef46eddba8af1f5be72de85bdc
DeproOs <klaudio_4ever@hotmail.com>
2023-05-25 20:23:08 -0400

    clase 59 terminada

--- HolaMundo/src/SentenciaSwitchCase.java
+++ HolaMundo/src/SentenciaSwitchCase.java
@@ -1,5 +1,55 @@
+import java.util.Scanner;
+
 public class SentenciaSwitchCase {
     public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        System.out.println("Ingrese el numero del mes");
+        int mes = s.nextInt();
+        String nombreMes = null;
+
+        switch (mes){
+            case 1 :
+                nombreMes = "Enero";
+                break;
+            case 2:
+                nombreMes = "Febrero";
+                break;
+            case 3:
+                nombreMes = "Marzo";
+                break;
+            case 4:
+                nombreMes = "Abril";
+                break;
+            case 5:
+                nombreMes = "Mayo";
+                break;
+            case 6:
+                nombreMes = "junio";
+                break;
+            case 7:
+                nombreMes = "julio";
+                break;
+            case 8:
+                nombreMes = "Agosto";
+                break;
+            case 9:
+                nombreMes = "Septiembre";
+                break;
+            case 10:
+                nombreMes = "Octubre";
+                break;
+            case 11:
+                nombreMes = "Noviembre";
+                break;
+            case 12:
+                nombreMes = "Diciembre";
+                break;
+            default:
+                nombreMes = "Indefinido";
+        }
+
+        System.out.println("nombreMes = " + nombreMes);
 
         char num = '3';
 
