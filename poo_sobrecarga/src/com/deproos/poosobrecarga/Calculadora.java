d77860035480154c714199d293cd8b6115f33402
DeproOs <klaudio_4ever@hotmail.com>
2023-07-21 18:18:39 -0400

    clase 178 terminada

--- poo_sobrecarga/src/com/deproos/poosobrecarga/Calculadora.java
+++ poo_sobrecarga/src/com/deproos/poosobrecarga/Calculadora.java
@@ -1,27 +1,52 @@
 package com.deproos.poosobrecarga;
 
 public class Calculadora {
-    public int sumar(int a, int b){
+    private Calculadora() {
+    }
+
+    public static int sumar(int a, int b){
         return a + b;
     }
 
-    public float sumar(float x, float y){
+    public static int sumar(int... argumentos){
+        int total = 0;
+        for (int argumento : argumentos) {
+            total = total + argumento;
+        }
+        return total;
+    }
+    public static float sumar(float a, int... argumentos){
+        float total = a;
+        for (int argumento : argumentos) {
+            total = total + argumento;
+        }
+        return total;
+    }
+    public static double sumar(double... varargs){
+        double total = 0;
+        for (double vararg : varargs) {
+            total = total + vararg;
+        }
+        return total;
+    }
+
+    public static float sumar(float x, float y){
         return x + y;
     }
 
-    public float sumar(int i, float j){
+    public static float sumar(int i, float j){
         return i + j;
     }
 
-    public float sumar(float i, int j){
+    public static float sumar(float i, int j){
         return i + j;
     }
 
-    public double sumar(double a, double b){
+    public static double sumar(double a, double b){
         return a + b;
     }
 
-    public int sumar(String a, String b){
+    public static int sumar(String a, String b){
         int resultado;
         try{
             resultado = Integer.parseInt(a) + Integer.parseInt(b);
@@ -31,7 +56,7 @@ public class Calculadora {
         return resultado;
     }
 
-    public int sumar(int a, int b, int c){
+    public static int sumar(int a, int b, int c){
         return a + b + c;
     }
 }
--- poo_sobrecarga/src/com/deproos/poosobrecarga/EjemploSobrecarga.java
+++ poo_sobrecarga/src/com/deproos/poosobrecarga/EjemploSobrecarga.java
@@ -1,21 +1,22 @@
 package com.deproos.poosobrecarga;
-
+import static com.deproos.poosobrecarga.Calculadora.*;
 public class EjemploSobrecarga{
     public static void main(String[] args) {
 
-        Calculadora cal = new Calculadora();
-
-        System.out.println("sumar int : " + cal.sumar(10, 5));
-        System.out.println("sumar float : " + cal.sumar(10.0f, 5f));
-        System.out.println("sumar float-int : " + cal.sumar(10f , 5));
-        System.out.println("sumar int-float : " + cal.sumar(10, 5.0f));
-        System.out.println("sumar double : " + cal.sumar(10.0 ,5.0));
-        System.out.println("sumar string : " + cal.sumar("10", "5"));
-        System.out.println("sumar 3 int : " + cal.sumar(10, 5, 3));
+        System.out.println("sumar int : " + sumar(10, 5));
+        System.out.println("sumar float : " + sumar(10.0f, 5f));
+        System.out.println("sumar float-int : " + sumar(10f , 5));
+        System.out.println("sumar int-float : " + sumar(10, 5.0f));
+        System.out.println("sumar double : " + sumar(10.0 ,5.0));
+        System.out.println("sumar string : " + sumar("10", "5"));
+        System.out.println("sumar 3 int : " + sumar(10, 5, 3));
+        System.out.println("sumar x int : " + sumar(10, 5, 3, 5, 6, 8, 95, 102));
+        System.out.println("sumar float + x int : " + sumar(10.5F, 5, 3, 5, 6, 8, 95, 102));
+        System.out.println("sumar 3 double : " + sumar(10.5, 5.5, 3.5));
 
         System.out.println();
-        System.out.println("suma Long : " + cal.sumar(10L, 5L));
-        System.out.println("suma int : " + cal.sumar(10, '@'));
-        System.out.println("sumar float-int : " + cal.sumar(10F, '@'));
+        System.out.println("suma Long : " + sumar(10L, 5L));
+        System.out.println("suma int : " + sumar(10, '@'));
+        System.out.println("sumar float-int : " + sumar(10F, '@'));
     }
 }
